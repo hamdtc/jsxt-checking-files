c7ca517c34ea1e6b123024c3ee01d15ef2f2bd7f
hamdtc <mohamedtc73@gmail.com>
2023-04-25 07:31:32 +0530

    Committing changes to fil.js before merge

--- fil.js
+++ fil.js
@@ -1,46 +1,19 @@
 
  const jsxl = require('jsxl');
-// jsxl({
-// 	input: {
-// 		array1: [ { a: 1, b: 'x' } , { a: 2, b: 'y' }, { a: 3, b: 'z' } ],
-// 		array2: [ 4, 5, 6 ],
-// 		array3: [ 'a','b','v'],
-// 	}},
-// 	{ // filter
-// 		array1: [ { a: Number }, { b: String } ],
-// 		array2: [ Number ],
-// 		array3: [ String ]
-// 	}, 
-// 	(err, output) => {
-// 		console.log(output);
+jsxl({
+	input: {
+		array1: [ { a: 1, b: 'x' } , { a: 2, b: 'y' }, { a: 3, b: 'z' } ],
+		array2: [ 4, 5, 6 ],
+		array3: [ 'a','b','v'],
+	}},
+	{ // filter
+		array1: [ { a: Number }, { b: String } ],
+		array2: [ Number ],
+		array3: [ String ]
+	}, 
+	(err, output) => {
+		console.log(output);
 		
-// 	}
-// );
+	}
+);
 
-const input = [
-  { number: 7 },
-  { number: 'a' },
-  { number: 9 }
-];
-
-const schema = [
-  {
-    $type: Number,
-    $transform: (context, value, next) => {
-      if (typeof value === 'number') {
-        next(null, value);
-      } else {
-        next(null, undefined);
-      }
-    }
-  }
-];
-
-jsxl(input, schema, (err, output) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(output);
-    // output: [{ number: 7}, {number: 9}]
-  }
-});
